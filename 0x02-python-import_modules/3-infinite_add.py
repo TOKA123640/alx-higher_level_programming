db4734b0e3655fcdf343fc55093781bf767ea48e
toka magdy <tokamagdy545@gmail.com>
2024-01-04 16:45:00 -0800

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-    argc = len(sys.argv) - 1
-
-    x = 0
+if __name__ == "__main__":
+    import sys
     result = 0
-for arg in sys.argv:
-    if x != 0:
-        result += int(arg)
-    else:
-        x += 1
-print("{:d}".format(result))
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
