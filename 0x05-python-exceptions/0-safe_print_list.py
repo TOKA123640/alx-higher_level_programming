50197c498cf89d8319aa7a26974a40a761370a96
toka magdy <tokamagdy545@gmail.com>
2024-01-22 14:59:06 -0800

    com

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ret = 0
+    i = 0
     num = 0
     for i in range(x):
         try:
@@ -9,4 +9,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (ret)
+    return num
