4bc65541060c4d7b167cd7d5bc0e34a8e77b707e
toka magdy <tokamagdy545@gmail.com>
2024-02-06 14:34:50 -0800

    inout

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""defining write_file with two argments"""
 
 def write_file(filename="", text=""):
+    """reads filename with utf-8"""
     with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
