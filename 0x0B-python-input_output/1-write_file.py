075983a51a71fa78631fb815122791254af8ae94
toka magdy <tokamagdy545@gmail.com>
2024-02-06 15:32:36 -0800

    input

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """defining write_file with two argments"""
 def write_file(filename="", text=""):
-    """reads filename with UTF-8"""
+    """reads filename with """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""defining load_from_json_file function"""
 
 import json
 
 
 def load_from_json_file(filename):
+    """creates an object from json file"""
     with open(filename, "r", encoding="UTF-8") as f:
         return json.load(f)
