c1b34056e968c3acd4f7ad6f683b538b8e44b0b9
toka magdy <tokamagdy545@gmail.com>
2024-02-06 16:03:10 -0800

    input

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """defining read_file function"""
 
 def read_file(filename=""):
-
+    """reads filename with utf-8"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
